bf6dde2614b40a6f463191a014c5cca54b2a953d
green-cabbage <hyeonseo_yun@hotmail.com>
2024-03-23 04:41:02 +0100

    adding bigger input file list for nanoevent factory implementation + dask gateway

--- memleakBig_test.py
+++ memleakBig_test.py
@@ -5,7 +5,7 @@ print(f"distributed verion: {distributed.__version__}")
 import numpy as np
 import awkward as ak
 import dask_awkward as dak
-# from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
+from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
 from distributed import Client, performance_report
 import json 
 import glob
@@ -34,7 +34,11 @@ def divide_chunks(data: dict, SIZE: int):
 if __name__ == '__main__':
     do_regular_restart = False
     # client = Client(n_workers=1,  threads_per_worker=1, processes=True, memory_limit='0.7 GiB')
-    gateway = Gateway()
+    # gateway = Gateway()
+    gateway = Gateway(
+        "http://dask-gateway-k8s.geddes.rcac.purdue.edu/",
+        proxy_address="traefik-dask-gateway-k8s.cms.geddes.rcac.purdue.edu:8786",
+    )
     cluster_info = gateway.list_clusters()[0]# get the first cluster by default. There only should be one anyways
     client = gateway.connect(cluster_info.name).get_client()
     # sample_path = "./input_file.json"
@@ -50,19 +54,22 @@ if __name__ == '__main__':
         smaller_files = list(divide_chunks(sample["files"], max_file_len))
         for idx in tqdm.tqdm(range(len(smaller_files)), leave=False):
             smaller_sample = copy.deepcopy(sample)
-            # print("test")
             smaller_sample["files"] = smaller_files[idx]
-            fnames = list(smaller_sample["files"].keys())
-            input = {fname: "Events" for fname in fnames}
-            events = uproot.dask(input, handler=uproot.XRootDSource)
-            muons_pt = events["Muon_pt"]
-            nmuons = ak.num(muons_pt, axis=1)
+            # fnames = list(smaller_sample["files"].keys())
+            # input = {fname: "Events" for fname in fnames}
+            events = NanoEventsFactory.from_root(
+                smaller_sample["files"],
+                schemaclass=NanoAODSchema,
+                metadata= smaller_sample["metadata"],
+                uproot_options={"handler" : uproot.XRootDSource}
+            ).events()
+            nmuons = ak.num(events.Muon, axis=1)
             muon_selection = (
-                muons_pt > 20 &
+                events.Muon.pt > 20 &
                 nmuons == 2
             )
-            muons_pt = muons_pt[muon_selection]
-            dask.compute(muons_pt)
+            muons = events.Muon[muon_selection]
+            dask.compute(muons.pt)
             if do_regular_restart:
                 client.restart(wait_for_workers=False)
             print(f"n_files_processed so far: {n_files_processed}")
